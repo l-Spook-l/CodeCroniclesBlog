38e7a99ef6afacd71755f0a470134106e14fe8e7
l-Spook-l <uaspookua@gmail.com>
2024-03-10 23:17:24 +0200

    Pagination by pages has been implemented.

--- blog/views.py
+++ blog/views.py
@@ -5,11 +5,18 @@ from .models import CategoryPost, Post, PostPhoto
 from .serializer import PostSerializer, CategorySerializer
 
 
+class PostAPILIstPagination(PageNumberPagination):
+    page_size = 3  # кол-во записей на 1й стр
+    page_query_param = 'page'
+    max_page_size = 1000  # Ограничение записей на 1й стр для - page_query_param = 'page_size'
+
+
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()  # Берем все записи из нужной табл в БД
     serializer_class = PostSerializer  # подключаем DRF
     # filterset_class = ProductFilter  # фильтрации записей модели "Product" на основе параметров запроса
     # filter_backends = [DjangoFilterBackend]
+    pagination_class = PostAPILIstPagination  # Пагинация
     lookup_field = 'slug'
 
 
@@ -17,3 +24,16 @@ class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = CategoryPost.objects.all()
     serializer_class = CategorySerializer
     lookup_field = 'slug'
+
+
+class PostListByCategory(viewsets.ReadOnlyModelViewSet):
+    serializer_class = PostSerializer
+    # filterset_class = CategoryFilter
+    # filter_backends = [DjangoFilterBackend]
+    pagination_class = PostAPILIstPagination
+
+    def get_queryset(self):
+        category_slug = self.kwargs['category_slug']
+        category = get_object_or_404(CategoryPost, slug=category_slug)
+        queryset = Post.objects.filter(category=category)
+        return queryset
