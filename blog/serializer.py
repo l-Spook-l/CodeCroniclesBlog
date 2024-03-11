8808a7e6cf1dece2878c5ac161316be4f9d288bc
l-Spook-l <uaspookua@gmail.com>
2024-03-11 13:30:41 +0200

    The ID fields for category and author have been replaced with their respective names and usernames.

--- blog/serializer.py
+++ blog/serializer.py
@@ -1,11 +1,15 @@
+# сериализатор - конвертирование python-кода в json и наоборот
 from rest_framework import serializers
-from .models import CategoryPost, Post, PostPhoto
+from .models import CategoryPost, Post, PostPhoto, Comment
 
 
 class PostSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(slug_field="name", read_only=True)
+    author = serializers.SlugRelatedField(slug_field="username", read_only=True)
+
     class Meta:
         model = Post
-        fields = "__all__"
+        fields = "__all__"  # поля которые нужны клиенту
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -14,4 +18,9 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'slug')
 
 
+class CommentSerializer(serializers.ModelSerializer):
+    username = serializers.StringRelatedField(source='user.username')
 
+    class Meta:
+        model = Comment
+        fields = "__all__"  # если надо все поля
